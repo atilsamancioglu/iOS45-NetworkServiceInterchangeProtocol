e1a228907ca5834c7304243781e82fb9a9a46823
Atil Samancioglu <samancioglu.atil@gmail.com>
2022-08-14 17:50:24 +0300

    Initial Commit

---
+++ NetworkServiceSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NetworkServiceSwiftUI
+//
+//  Created by Atil Samancioglu on 14.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NetworkServiceSwiftUI/NetworkServiceSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkServiceSwiftUIApp.swift
+//  NetworkServiceSwiftUI
+//
+//  Created by Atil Samancioglu on 14.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct NetworkServiceSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
